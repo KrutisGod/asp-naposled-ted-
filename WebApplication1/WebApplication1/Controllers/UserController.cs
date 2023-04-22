029104a6c9868c79adb11dd12fa92a4e21599a8e
KrutisGod <krutis@spstrutnov.cz>
2023-04-22 14:44:54 +0200

    scuffed start, user register

---
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MaturitaPvaCviceniASP.Models;
+using BCrypt;
+using System;
+
+namespace MaturitaPvaCviceniASP.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginPost()
+        {
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterPost(string username, string password, string passwordCheck)
+        {
+            if (username.Trim() == "" || password == "" || passwordCheck == "" || password != passwordCheck)
+            {
+                return RedirectToAction("register");
+            }
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            User newUser = new User { Username = username, Password = hashedPassword };
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaturitaPvaCviceniASP.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public virtual User Username { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public bool Starred { get; set; }
+    }
+}
